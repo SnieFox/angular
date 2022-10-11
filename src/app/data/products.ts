cfe6989186614b34c8aa1bc3984689698f74c8f8
Данііл Огороднік <D_Ohorodnik_FOAIS_19_6_B_d@knute.edu.ua>
2022-10-11 11:49:01 +0300

    2 commit

---
+++ src/app/data/products.ts
@@ -0,0 +1,29 @@
+import {IProduct} from "../models/product";
+
+
+export const products: IProduct[] = [
+  {
+    id:1,
+    title:'Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops',
+    price:109.95,
+    category:'men\'s clothing',
+    description:'Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday',
+    image:'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
+    rating: {
+      rate: 3.9,
+      count: 120
+    }
+  },
+  {
+    id:2,
+    title:'dsffffffffffff',
+    price:10900.95,
+    category:'men\'sss clothing',
+    description:'Your perfect dick for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday',
+    image:'https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg',
+    rating: {
+      rate: 4.1,
+      count: 259
+    }
+  }
+]
